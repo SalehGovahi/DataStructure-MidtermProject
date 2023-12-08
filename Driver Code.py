6ab12ca6dd001fb55d3617bb50e6c47196718010
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2023-12-08 18:28:46 +0330

    Add base class to run all of project

---
+++ Driver Code.py
@@ -0,0 +1,8 @@
+from WaterSortGame import *
+
+input_string = input("Enter your colors: ")
+colors_list = input_string.split()
+print(colors_list)
+input_string = input("Enter maximum size of a battle: ")
+battle_max_size = int(input_string)
+print(battle_max_size)
